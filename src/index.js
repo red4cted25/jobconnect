4d35f887e121c9313f574a4ed7b4560a528b1337
Nico <141285419+red4cted25@users.noreply.github.com>
2025-01-08 18:22:18 -0700

    Created React Project
    Created the react project to begin coding the pages for JobConnect

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+])
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
