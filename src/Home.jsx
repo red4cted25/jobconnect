7c1fdd4d407f6c9807b57332bc180f7ef1ba2a8b
Nico <141285419+red4cted25@users.noreply.github.com>
2025-01-17 15:23:27 -0700

    Mobile Header, Mobile Home
    ---------- Home ----------
    Other than an issue with the footer on mobile, the Home page is all complete and working for mobile. Sizing is appropriate, and everything looks professional
    
    ---------- Header ----------
    Everything should be working, the mobile nav works, but more needs to be done with figuring out dropdowns in the mobile menu and changing all the Links as well as adding more Links from react router dom

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,46 +12,47 @@ const HomePage = () => {
       <main className='w-screen h-[calc(100vh-5rem)] flex flex-col'>
         {/* Hero Section */}
         {/* The hero section is a video of Northeast Campus and a gradient overlay with a tagline */}
-        <section className='w-full h-3/5 flex flex-col justify-center bg-gradient-to-tr from-brand-primary via-brand-dark-primary to-brand-primary font-Golos font-extrabold'>
+        <section className='w-full h-2/5 md:h-3/5 flex flex-col justify-center bg-gradient-to-tr from-brand-primary via-brand-dark-primary to-brand-primary font-Golos font-extrabold'>
           <div className='w-fit text-brand-dark-primary rounded-r-2xl bg-brand-dark-gray bg-opacity-50 pl-8 p-6'>
-            <h1 className="text-5xl leading-hero italic drop-shadow-text">FIND YOUR <br />ARROW</h1>
-            <h2 className="text-xl text-white drop-shadow-text-caption mt-4">THIS IS GENERATION <span className="text-brand-dark-primary">ORANGE</span></h2>
+            <h1 className="text-5xl leading-hero italic drop-shadow-text md:text-7xl">FIND YOUR <br />ARROW</h1>
+            <h2 className="text-xl text-white drop-shadow-text-caption mt-4 md:text-2xl">THIS IS GENERATION <span className="text-brand-dark-primary">ORANGE</span></h2>
           </div>
         </section>
         {/* Page Links */}
         {/* All of these lead to the /jobs page */}
         {/* They all have different effects on the filter system in place to serve as quick links to specific searches */}
-        <section className='flex flex-grow justify-around items-center text-center mx-6'>
+        <section className='flex grow-[0.25] justify-around items-center text-center mt-4 mx-6 flex-wrap'>
           {/* Apply */}
           <Link to="/jobs">
-            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4">
-              <LuNotebookPen className='w-36 h-36'/>
-              <h3 className="text-xl">Apply</h3>
+            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4 size-40 md:size-48">
+              <LuNotebookPen className='size-3/4 mx-auto'/>
+              <h3 className="text-xl md:text-2xl font-bold">Apply</h3>
             </div>
           </Link>
           {/* Alumni */}
           <Link to="/jobs">
-            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4">
-              <FaGraduationCap className='w-36 h-36'/>
-              <h3 className="text-xl">Alumni</h3>
+            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4 size-40 md:size-48">
+              <FaGraduationCap className='size-3/4 mx-auto'/>
+              <h3 className="text-xl md:text-2xl font-bold">Alumni</h3>
             </div>
           </Link>
           {/* Activities */}
           <Link to="/jobs">
-            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4">
-              <FaRegCalendarCheck className='w-36 h-36'/>
-              <h3 className="text-xl">Activities</h3>
+            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4 size-40 md:size-48">
+              <FaRegCalendarCheck className='size-3/4 mx-auto'/>
+              <h3 className="text-xl md:text-2xl font-bold">Activities</h3>
             </div>
           </Link>
           {/* Scholarships */}
           <Link to="/jobs">
-            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4">
-              <FaMoneyBills className='w-36 h-36'/>
-              <h3 className="text-xl">Scholarships</h3>
+            <div className="bg-brand-light-gray flex flex-col justify-center p-6 gap-4 size-40 md:size-48">
+              <FaMoneyBills className='size-3/4 mx-auto'/>
+              <h3 className="text-xl md:text-2xl font-bold">Scholarships</h3>
             </div>
           </Link>
         </section>
       </main>
+      <footer className="w-full h-16 bg-brand-secondary relative md:hidden"></footer>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
 const Header = () => {
-    const [toggleOpen, setToggleOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined
+    })
 
-    const handleToggle = () => setToggleOpen(!toggleOpen);
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+        }
+        window.addEventListener('resize', handleResize)
 
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+        // Adjust menu width based on screen size
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false)
+        }
+    }, [size.width, menuOpen])
+    
+
+    const menuToggleHandler = () => {
+        setMenuOpen(!menuOpen);
+    }
 
     return (
         // <header className='flex h-20 bg-brand-secondary text-white items-center'>
@@ -25,8 +50,6 @@ const Header = () => {
         //                 <Link to="/jobconnect/faq" className='hover:underline underline-offset-8 block text-white py-2 px-4 text-left'>FAQ</Link>
         //             </div>
         //         </div>
-        //         <Link to="/companies" className='hover:underline underline-offset-8 text-center px-4'>Companies</Link>
-        //         <Link to="/jobs" className='hover:underline underline-offset-8 text-center px-4'>Jobs</Link>
         //         <div className='relative float-left group'>
         //             <button className="hover:underline underline-offset-8 border-none outline-none text-white py-3.5 px-4 bg-transparent font-inherit m-0">Community</button>
         //             <div className="hidden group-hover:block absolute bg-brand-secondary min-w-auto shadow-dropdown z-10">
@@ -36,7 +59,6 @@ const Header = () => {
         //                 <Link to="#" className='hover:underline underline-offset-8 block text-white py-2 px-4 text-left whitespace-nowrap'>Campus Activities</Link>
         //             </div>
         //         </div>
-        //         <Link to="#" className='hover:underline underline-offset-8 text-center px-4'>For Employers</Link>
         //         {/* Sign In / Log In */}
         //         {/* Switches to show username and profile picture when user is signed in */}
         //         <div>
@@ -45,27 +67,35 @@ const Header = () => {
         //     </nav>
         // </header>
         <header className="bg-brand-secondary w-full h-20 px-6 md:px-12 transition-all ease-linear duration-300">
-            <div className="overflow-hidden text-white mx-auto max-w-7xl h-full flex items-center justify-between relative z-50">
+            <div className="overflow-hidden text-white max-w-full h-full flex items-center justify-between relative z-50">
                 {/* Logo */}
                 <Link to='/' className='flex flex-col font-semibold items-end'>
                     <h1 className='ml-4 text-2xl'>Job<span className='text-brand-primary'>Connect</span></h1>
                     <h2 className='-mt-1'>@ West-<span className='text-brand-primary'>MEC</span></h2>
                 </Link>
-                <nav className="top-0 right-full w-full h-screen fixed flex flex-col justify-center items-center text-center bg-black opacity-90 backdrop-blur-sm translate-x-0 translate-y-0 transition-transform duration-300 ease-linear md:transform-none md:flex-row md:bg-transparent md:w-auto md:h-full md:static">
-                    <ul className='list-none p-0 flex flex-col mb-spacing-lg md:flex-row md:items-center md:mb-0 md:mr-[calc(0.5rem-2rem)]'>
+                <nav className={`top-0 right-full w-full h-screen fixed flex flex-col justify-center items-center text-center bg-black opacity-90 backdrop-blur-sm translate-x-0 translate-y-0 transition-transform duration-300 ease-linear md:transform-none md:flex-row md:bg-transparent md:w-auto md:h-full md:static ${menuOpen ? 'translate-x-full' : ''}`}>
+                    <ul className='list-none p-0 flex flex-col mb-spacing-lg md:flex-row md:items-center md:mb-0 md:mr-6'>
                         <li className='mb-spacing-lg md:mb-0 md:mr-spacing-md'>
                             <Link to='/' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>PageOne</Link>
                         </li>
                         <li className='mb-spacing-lg md:mb-0 md:mr-spacing-md'>
-                            <Link to='/' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>PageTwo</Link>
+                            <Link to='/' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>PageOne</Link>
+                        </li>
+                        <li className='mb-spacing-lg md:mb-0 md:mr-spacing-md'>
+                            <Link to='/jobs' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>Jobs</Link>
+                        </li>
+                        <li className='mb-spacing-lg md:mb-0 md:mr-spacing-md'>
+                            <Link to='/companies' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>Companies</Link>
                         </li>
                         <li>
-                            <Link to='/' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>PageThree</Link>
+                            <Link to='/employers' className='no-underline text-inherit py-3 px-5 rounded-nav-button transition-all duration-300 ease-linear hover:underline underline-offset-8'>For Employers</Link>
                         </li>
                     </ul>
-                    <button className='cursor-pointer outline-none py-3 px-5 rounded-nav-button text-base bg-brand-primary text-white transition-all ease-linear duration-300 hover:bg-brand-dark-primary active:bg-gradient-to-tr active:from-brand-dark-primary active:to-brand-primary'>Sign In</button>
+                    <button className='cursor-pointer outline-none py-3 px-5 rounded-nav-button text-base bg-brand-primary text-white transition-all ease-linear duration-300 hover:bg-brand-dark-primary active:bg-gradient-to-tr active:from-brand-primary active:via-brand-dark-primary active:to-brand-primary'>Sign In</button>
                 </nav>
-                div.
+                <div className="cursor-pointer flex items-center text-3xl transition-all ease-linear duration-300 relative hover:text-brand-primary md:hidden">
+                    {!menuOpen ? <RxHamburgerMenu onClick={menuToggleHandler}/> : <RxCross1 onClick={menuToggleHandler} />}
+                </div>
             </div>
         </header>
     )
